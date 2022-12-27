0b6d8b3b4004ac5f74340761cb58e40cbf4a6371
mooventhan-k <mooventhan@progton.com>
2022-12-27 17:15:39 +0530

    before initializers

---
+++ db/migrate/20221227110946_create_author.rb
@@ -0,0 +1,13 @@
+class CreateAuthor < ActiveRecord::Migration[7.0]
+  def change
+    create_table :authors do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    create_table :authors_books, id: false do |t|
+      t.belongs_to :author
+      t.belongs_to :book
+    end
+  end
+end
